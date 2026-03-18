ea073cf83862296181c45de011ab79f782b7b55f
Clint Laurence Englis <clintenglis16@gmail.com>
2026-03-18 11:59:11 +0800

    Activity A

--- app/src/main/java/com/example/englis_cis2203n_exercise03/MainActivity.java
+++ app/src/main/java/com/example/englis_cis2203n_exercise03/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.englis_cis2203n_exercise03;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Log.d(TAG, "onCreate method has successfully started.");
+
+        EditText editMessage = findViewById(R.id.editMessage);
+        Button btnSend = findViewById(R.id.btnSend);
+
+        btnSend.setOnClickListener(v -> {
+            String message = editMessage.getText().toString();
+            Log.d(TAG, "Send button clicked with message: " + message);
+
+            Intent intent = new Intent(MainActivity.this, EchoActivity.class);
+            intent.putExtra("EXTRA_MESSAGE", message);
+            startActivity(intent);
         });
     }
 }
